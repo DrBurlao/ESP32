fcfcc8dc2e093aaa9098912e0943c6616f9196cd
Dr.Burlao <134441445+DrBurlao@users.noreply.github.com>
2024-08-25 00:33:45 +0200

    Update 3x3LedMatrixV2.ino

--- 3x3LedMatrix/3x3LedMatrixV2.ino
+++ 3x3LedMatrix/3x3LedMatrixV2.ino
@@ -5,9 +5,9 @@
 #define LED_BLANCO_1 27
 #define LED_AZUL_2 16
 #define LED_BLANCO_2 17
-#define LED_CENTRAL_R 13
+#define LED_CENTRAL_R 2
 #define LED_CENTRAL_G 12
-#define LED_CENTRAL_B 2
+#define LED_CENTRAL_B 13
 #define LED_BLANCO_3 25
 #define LED_AZUL_3 26
 #define LED_BLANCO_4 1
